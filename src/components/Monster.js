41cda9c95552ddbbbd31a73651d227fc9d8390c9
Justin <Justin.Swatloski@gmail.com>
2022-11-13 22:28:37 -0600

    Functional click-to-delete list

--- src/components/Monster.js
+++ src/components/Monster.js
@@ -2,8 +2,12 @@ import './Monster.css';
 
 const Monster = (props) => {
 
+   const deleteHandler = () => {
+        props.onDeleteClick(props.name);
+    };
+
     return(
-        <div className='monster'>
+        <div className='monster' onClick={deleteHandler}>
     <p>Name: {props.name}    |    Strength: {props.strength}</p>
     
     </div>
