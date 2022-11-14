32e4f1022b4baed4d7eea36262697074865f794d
Justin <Justin.Swatloski@gmail.com>
2022-11-14 17:48:10 -0600

    Added "add x sided die" feature

--- src/components/Dice/Dicebag.js
+++ src/components/Dice/Dicebag.js
@@ -1,14 +1,39 @@
 import Die from "./Die";
+import { useState } from "react";
+
+const basicDiceSet = [6, 8, 10,20,100];
 
 const Dicebag = () => {
 
+    const[dice, setDice] = useState(basicDiceSet);
+    const[enteredX, setEnteredX] = useState("");
+
+    const changeHandler = (event) =>{
+        setEnteredX(event.target.value);
+    }
+    const addDie = () =>{
+        
+        setDice(prevDice => {
+            const updatedDice = [enteredX, ...prevDice];
+            return updatedDice;
+        });
+    };
+
     return(
         <div>
-            <Die sides="6"/>
-            <Die sides="8"/>
-            <Die sides="10"/>
-            <Die sides="20"/>
-            <Die sides="100"/>
+            {dice.map(die => (
+                <Die key={die} sides={die} />
+            ))}
+            <button onClick={addDie}>Add X Die</button>
+            <input
+                        type='number'
+                        min="0"
+                        max="9999"
+                        value={enteredX}
+                        onChange={changeHandler}
+                        ></input>
+            
+            
         </div>
     );
         
