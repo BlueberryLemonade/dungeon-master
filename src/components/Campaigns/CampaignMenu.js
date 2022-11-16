df839b0c7db37a90f33f2b7cafc8c6fedb470279
Justin <Justin.Swatloski@gmail.com>
2022-11-15 23:45:57 -0600

    Working Router to Monsters

--- src/components/Campaigns/CampaignMenu.js
+++ src/components/Campaigns/CampaignMenu.js
@@ -1,18 +1,25 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import CampaignCreator from "./CampaignCreator";
 
 const CampaignMenu = (props) => {
 
 const[campaigns, setCampaigns] = useState(props.campaigns);
 
 const newCampaign = () => {
+
     setCampaigns("1");
 }
-
+<route></route>
 const MenuGeneration = () => {
     if(campaigns.length === 0){
 
        return (<div>
        <p>No Campaigns</p>
+
+       <Router>
+        <Route exact path="src\components\Campaigns\CampaignMenu.js" component={CampaignCreator} />
+       </Router>
        <button onClick={newCampaign}>Create a Campaign</button>
        </div>
        )
