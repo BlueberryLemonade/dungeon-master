9aa85dea662079aec73841ca04703be350a087a1
Justin <Justin.Swatloski@gmail.com>
2022-11-24 15:43:14 -0600

    Champion fetching from SQL

--- src/components/Champions/ChampionList.js
+++ src/components/Champions/ChampionList.js
@@ -1,18 +1,38 @@
 import Champion from "./Champion";
-const ChampionList = props => {
+import { useState, useEffect } from "react";
+const ChampionList = (props) => {
 
-    if (props.champions.length === 0) {
+    const [champions, setChampions] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+
+        fetch('http://localhost:8080/api/champions')
+        .then(response => response.json())
+        .then(data=> {
+            setChampions(data);
+            setLoading(false);
+        })
+    }, []);
+
+    if(loading){
+        return <p>Loading...</p>
+    }
+
+
+    if (champions.length === 0) {
         return <h2>No Champions in database</h2>
     }
 
     return (
         <ul>
-            {props.champions.map((champion) => (
+            {champions.map((champion) => (
                 <Champion
                     onDeleteClick={props.onDeleteClick}
                     key={champion.name}
                     name={champion.name}
-                    strength={champion.strength}
+                    strength={champion.hp}
                 />
             ))}
         </ul>
